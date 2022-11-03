377e22947171a48793e2cafe63901b25f1aba860
Diogo <diogobailaoforte@gmail.com>
2022-11-03 17:23:37 +0000

    overcheck

---
+++ Scripts/checkout.js
@@ -0,0 +1,18 @@
+histable.addEventListener('click', function(e){
+    if(e.target != this){
+      checktable.appendChild(e.target.cloneNode(true));
+      e.target.remove()
+    }
+  })
+  
+checktable.addEventListener('click', function(e){
+    if(e.target != this){
+      histable.appendChild(e.target.cloneNode(true));
+      e.target.remove()
+    }
+  })
+
+
+  .tables ;{
+    display: flex
+  };
