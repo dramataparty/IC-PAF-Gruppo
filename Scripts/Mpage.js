0c9b4731aac0ea29393ffa431aa3eb04bbdc34ec
Diogo <diogobailaoforte@gmail.com>
2022-12-05 16:37:06 +0000

    stuff

--- Scripts/Mpage.js
+++ Scripts/Mpage.js
@@ -1,1 +1,28 @@
 /// linkar botão de inicio , display etc a js que dê "display" do que importa
+function artigos_pedido() {
+    if (typeof(Storage) !== "undefined") {
+        const artigos = [];
+    }
+    if (sessionStorage.artigos) {
+        sessionStorage.artigos = sessionStorage(sessionStorage.artigos)+;
+
+
+      }
+
+
+
+  }
+
+
+  function addEntry() {
+    var entryTitle = document.getElementById("entryTitle").value;
+    var entryText = document.getElementById("entryText").value;
+    var entry = {
+        "title": entryTitle,
+        "text": entryText
+    };
+    localStorage.setItem("entry", JSON.stringify(entry));
+    var allEntries = [];
+    allEntries.push(entry);
+    localStorage.setItem("allEntries", JSON.stringify(allEntries));
+};
