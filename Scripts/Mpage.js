720d1b123b3c211d3d6089dd1faefe07e124e0c6
Diogo <diogobailaoforte@gmail.com>
2022-12-05 17:04:53 +0000

    artigosa

--- Scripts/Mpage.js
+++ Scripts/Mpage.js
@@ -1,28 +1,36 @@
 /// linkar botão de inicio , display etc a js que dê "display" do que importa
-function artigos_pedido() {
-    if (typeof(Storage) !== "undefined") {
-        const artigos = [];
+const histpedidos = []
+
+function currpedido_pedido() {
+    var currpedido = JSON.parse(localStorage.getItem("currpedido"));
+    if (currpedido == null){
+        currpedido = [];
     }
-    if (sessionStorage.artigos) {
-        sessionStorage.artigos = sessionStorage(sessionStorage.artigos)+;
-
-
-      }
-
-
-
-  }
-
-
-  function addEntry() {
-    var entryTitle = document.getElementById("entryTitle").value;
-    var entryText = document.getElementById("entryText").value;
-    var entry = {
-        "title": entryTitle,
-        "text": entryText
-    };
-    localStorage.setItem("entry", JSON.stringify(entry));
-    var allEntries = [];
-    allEntries.push(entry);
-    localStorage.setItem("allEntries", JSON.stringify(allEntries));
+    var artitle = document.getElementById("item")
+    currpedido.push(artitle);    
 };
+
+function final_pedido(){
+    histpedidos.push(currpedido)
+
+}
+
+pastables = document.getElementById('pastables');
+checktables = document.getElementById('checktables');
+
+checktables.addEventListener('click', function(e){
+    if(e.target != this){
+        pastables.appendChild(e.target.cloneNode(true));
+        e.target.remove()
+    }
+  })
+
+
+
+pastables.addEventListener('click', function(e){
+    if(e.target != this){
+      checktables.appendChild(e.target.cloneNode(true));
+      e.target.remove()
+    }
+  })
+  
