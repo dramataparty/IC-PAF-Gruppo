529aeb326ac6d564b485864fcf51e04e9bfd7efe
Diogo <diogobailaoforte@gmail.com>
2022-12-08 21:10:54 +0000

    s t u ff updated

---
+++ Scripts/pratos.js
@@ -0,0 +1,104 @@
+function appleCrumbleInfo() {
+    alert("Apple Crumble is a dessert made of apples, sugar, cinnamon, and a topping of oats and butter.");
+}
+
+function carrotCakeInfo() {
+    alert("Carrot cake is a cake flavored with carrots, cinnamon, and nutmeg.");
+}
+
+function cheesecakeInfo() {
+    alert("Cheesecake is a sweet dessert consisting of one or more layers. The main, and thickest layer, consists of a mixture of soft, fresh cheese, eggs, and sugar.");
+}
+
+function cremeBruleeInfo() {
+    alert("Crème brûlée is a dessert consisting of a rich custard base topped with a contrasting layer of hard caramel.");
+}
+
+function choclateMouseInfo() {
+    alert("Chocolate mousse is a light, airy, and fluffy chocolate dessert.");
+}
+
+function petitGateauInfo() {
+    alert("Petit gâteau is a small cake or cupcake.");
+}
+
+function waterInfo() {
+    alert("Water is a clear, colorless, odorless, tasteless liquid that is the main constituent of Earth's streams, lakes, and oceans, and the fluids of most living organisms.");
+}
+
+function coffeeInfo() {
+    alert("Coffee is a brewed drink prepared from roasted coffee beans, the seeds of berries from certain Coffea species.");
+}
+
+function whiskeyInfo() {
+    alert("Whiskey is a type of distilled alcoholic beverage made from fermented grain mash.");
+}
+
+function wineInfo() {
+    alert("Wine is an alcoholic beverage made from fermented grapes.");
+}
+
+function iceTeaInfo() {
+    alert("Ice tea is a sweetened tea drink, usually served chilled.");
+}
+
+function cocacolaInfo() {
+    alert("Coca-Cola is a carbonated soft drink produced by The Coca-Cola Company.");
+}
+
+function grapeJuiceInfo() {
+    alert("Grape juice is a juice made from grapes.");
+}
+
+function orangeJuiceInfo() {
+    alert("Orange juice is a liquid extract of the orange tree, produced by squeezing or reaming oranges.");
+}
+
+function fantaInfo() {
+    alert("Fanta is a brand of fruit-flavored carbonated soft drinks created by The Coca-Cola Company.");
+}
+
+function redFruitsJuiceInfo() {
+    alert("Red fruits juice is a juice made from red fruits.");
+}
+
+function lemonadeInfo() {
+    alert("Lemonade is a sweetened lemon-flavored drink.");
+}
+
+function onionBaconBurgerLettuceInfo() {
+    alert("Onion Bacon Burger Lettuce is a burger with onion, bacon, and lettuce.");
+}
+
+function royalBurgerInfo() {
+    alert("Royal Burger is a burger with onion, bacon, and cheese.");
+}
+
+function chickenSaladInfo() {
+    alert("Chicken Salad is a salad with chicken.");
+}
+
+function paellaInfo() {
+    alert("Paella is a Spanish rice dish.");
+}
+
+function porkRiceEggInfo() {
+    alert("Pork Rice Egg is a rice dish with pork and egg.");
+}
+
+function steakBroccoliPotatoInfo() {
+    alert("Steak Broccoli Potato is a dish with steak, broccoli, and potato.");
+}
+
+function shrimpRisottoInfo() {
+    alert("Shrimp Risotto is a risotto with shrimp.");
+}
+
+function seardSalmonInfo() {
+    alert("Seard Salmon is a dish with salmon.");
+}
+
+function bacalhauNatasInfo() {
+    alert("Bacalhau Natas is a dish with codfish and cream.");
+}
+    
